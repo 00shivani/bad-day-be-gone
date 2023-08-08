a28a3e821ccb8e7160f3a0e8f97464d60b061b3b
00shivani <shivanisingh13@icloud.com>
2023-08-07 21:21:32 -0400

    added nav bar, playing around with tailwind

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,16 @@
 import "tailwindcss/tailwind.css"
+
+// tasks:
+// 1. create div
+// 2. keep appending new content to div
+// 3. animals: 
+// click animals activity box  on home screen
+// box fills body html space
+// generate animal button:
+// image container at the bottom that appends image
+// exit button at the bottom
+
+// quotes
+// same thing: click on home screen box, takes up whole screen except nav and logo and above
+// click generate
+// exit button
